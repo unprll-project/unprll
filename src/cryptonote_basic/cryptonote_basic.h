3c3765518fdf5814bb7f91678d974760b0985c3a
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2018-11-30 14:54:46 +0000

    cryptonote: add a set_null for transaction_prefix
    Since it's all inline, I suspect the compiler will merge the
    duplicate stores anyway.

--- src/cryptonote_basic/cryptonote_basic.h
+++ src/cryptonote_basic/cryptonote_basic.h
@@ -177,7 +177,15 @@ namespace cryptonote
     END_SERIALIZE()
 
   public:
-    transaction_prefix(){}
+    transaction_prefix(){ set_null(); }
+    void set_null()
+    {
+      version = 1;
+      unlock_delta = 0;
+      vin.clear();
+      vout.clear();
+      extra.clear();
+    }
   };
 
   class transaction: public transaction_prefix
@@ -304,17 +312,12 @@ namespace cryptonote
   inline
   transaction::~transaction()
   {
-    //set_null();
   }
 
   inline
   void transaction::set_null()
   {
-    version = 1;
-    unlock_delta = 0;
-    vin.clear();
-    vout.clear();
-    extra.clear();
+    transaction_prefix::set_null();
     signatures.clear();
     rct_signatures.type = rct::RCTTypeNull;
     set_hash_valid(false);
