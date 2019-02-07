5c6ad2a45babc3a44e229f87584d9d70c0305846
Christopher Tobias <ctobias@gmx.com>
2019-02-07 23:13:56 +0530

    Update simplewallet message

--- src/simplewallet/simplewallet.cpp
+++ src/simplewallet/simplewallet.cpp
@@ -440,7 +440,7 @@ namespace
       {
         writer << "\n" << sw::tr("output amount") << " = " << print_money(outs_for_amount.first) << ", " << sw::tr("found outputs to use") << " = " << outs_for_amount.second;
       }
-      writer << sw::tr("Please use sweep_unmixable.");
+      writer << sw::tr("Please try again.");
     }
     catch (const tools::error::tx_not_constructed&)
     {
