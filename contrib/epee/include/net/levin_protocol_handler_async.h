a26d1f63609025eaae7dd8807116614507b1ed2b
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2019-04-22 22:29:56 +0000

    levin_protocol_handler_async: tune down preallocation a fair bit
    It can allocate a lot when getting a lot of connections
    (in particular, the stress test on windows apparently pushes
    that memory to actual use, rather than just allocated)

--- contrib/epee/include/net/levin_protocol_handler_async.h
+++ contrib/epee/include/net/levin_protocol_handler_async.h
@@ -264,9 +264,9 @@ public:
     t_connection_context& conn_context):
             m_current_head(bucket_head2()),
             m_pservice_endpoint(psnd_hndlr),
-            m_config(config), 
-            m_connection_context(conn_context),
-            m_cache_in_buffer(256 * 1024),
+            m_config(config),
+            m_connection_context(conn_context), 
+            m_cache_in_buffer(4 * 1024),
             m_state(stream_state_head)
   {
     m_close_called = 0;
